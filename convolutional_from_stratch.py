ff5388e60f4a098374daefe462814808741693ff
Ruofeng <geo0105824@gmail.com>
2019-02-08 15:59:05 +0000

    1st draft of working one hidden layer convolution network, working on  MNIST.

--- convolutional_from_stratch.py
+++ convolutional_from_stratch.py
@@ -1,21 +1,34 @@
 import numpy as np
 from scipy import signal
 
+# np.random.seed(598765)
+
 
 def tanh(x):
     return np.tanh(x)
-
-
 def d_tanh(x):
     return 1 - np.tanh(x) ** 2
 
-
-def log(x):
+def sigmoid(x):
     return 1 / (1 + np.exp(-1 * x))
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
+def ReLu(x):
+    return np.maximum(x, 0)
+def d_ReLu(x):
+    return (x > 0) * 1.0
 
-def d_log(x):
-    return log(x) * (1 - log(x))
+def softmax(x):
+    exps = np.exp(x)
+    return exps / np.sum(exps)
+def softmax_grad(softmax):
+    # Reshape the 1-d softmax to 2-d so that np.dot will do the matrix multiplication
+    s = softmax.reshape(-1,1)
+    return np.diagflat(s) - np.dot(s, s.T)
+def d_softmax(softmax, x):
+    jacobian = softmax_grad(softmax)
+    return jacobian.dot(x.T)
 
 def loadMNIST( prefix, folder ):
     intType = np.dtype( 'int32' ).newbyteorder( '>' )
@@ -34,106 +47,120 @@ def mnist_data():
     testImages, testLabels = loadMNIST( "t10k", "." )
     return trainingImages, trainingLabels
 
-nb_classes = 10
-def indices_to_one_hot(data, nb_classes):
+def indices_to_one_hot(data, nb_classes=10):
     """Convert an iterable of indices to one-hot encoded labels."""
     targets = np.array(data).reshape(-1)
     return np.eye(nb_classes)[targets]
 
-
-np.random.seed(598765)
-
-x1 = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-x2 = np.array([[1, 1, 1], [0, 0, 0], [0, 0, 0]])
-x3 = np.array([[0, 0, 0], [1, 1, 1], [1, 1, 1]])
-x4 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-X = [x1, x2, x3, x4]
-Y = np.array([
-    [0.53],
-    [0.77],
-    [0.88],
-    [1.1]
-])
+train_X, train_y = mnist_data()
+train_X = train_X /255
+train_y = indices_to_one_hot(train_y)
+train_X = train_X[:1000]
+train_y = train_y[:1000]
 
 # 0. Declare Weights
-w1 = np.random.randn(2, 2) * 4
-w2 = np.random.randn(4, 1) * 4
+w1 = np.random.randn(3, 3, 3)*3
+w2 = np.random.randn(14*14*3,10)
+number_mask = w1.shape[0]
+
 
 # 1. Declare hyper Parameters
-num_epoch = 1
-learning_rate = 0.7
-
+num_epoch = 1000
+learning_rate = 0.2
+c_value = [0.2*i for i in range(15)]
 cost_before_train = 0
 cost_after_train = 0
-final_out, start_out = np.array([[]]), np.array([[]])
-
+# final_out, start_out = np.array([[]]), np.array([[]])
+layer_1 = np.zeros((14, 14, 3))
 # ---- Cost before training ------
-for i in range(len(X)):
-    layer_1 = signal.convolve2d(X[i], w1, 'valid')
-    layer_1_act = tanh(layer_1)
+for i in range(len(train_X)):
+    for j in range(number_mask):
+        layer_1[:, :, j] = signal.convolve2d(train_X[i], w1[:, :, j], mode='same')[::2,::2]
+    layer_1_act = sigmoid(layer_1)
 
     layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+
     layer_2 = layer_1_act_vec.dot(w2)
-    layer_2_act = log(layer_2)
-    cost = np.square(layer_2_act - Y[i]).sum() * 0.5
+    layer_2_act = softmax(layer_2)
+    cost = np.square(layer_2_act - train_y[i]).sum() * 0.5
     cost_before_train = cost_before_train + cost
-    start_out = np.append(start_out, layer_2_act)
+    # start_out = np.append(start_out, layer_2_act)
 
 # ----- TRAINING -------
 for iter in range(num_epoch):
-
-    for i in range(len(X)):
-        layer_1 = signal.convolve2d(X[i], w1, 'valid')
-        layer_1_act = tanh(layer_1)
+    if iter%300==0:
+        learning_rate = learning_rate *0.5
+    # print("------------iteration:", iter, "-----------")
+    for i in range(len(train_X)):
+        for j in range(number_mask):
+            layer_1[:, :, j] = signal.convolve2d(train_X[i], w1[:, :, j], mode='same')[::2, ::2]
+        layer_1_act = sigmoid(layer_1)
 
         layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+
         layer_2 = layer_1_act_vec.dot(w2)
-        layer_2_act = log(layer_2)
+        layer_2_act = softmax(layer_2)
 
-        cost = np.square(layer_2_act - Y[i]).sum() * 0.5
-        # print("Current iter : ",iter , " Current train: ",i, " Current cost: ",cost,end="\r")
+        cost = np.square(layer_2_act - train_y[i]).sum() * 0.5
 
-        grad_2_part_1 = layer_2_act - Y[i]
-        grad_2_part_2 = d_log(layer_2)
+        # if i == 999:
+        #     print("Current iter : ",iter , " Current train sample: ",i, " Current cost: ",cost )
+
+        grad_2_part_1 = layer_2_act - train_y[i]
+        # grad_2_part_2 = d_sigmoid(layer_2)
+        grad_2_part_2 = softmax_grad(layer_2_act)
         grad_2_part_3 = layer_1_act_vec
-        grad_2 = grad_2_part_3.T.dot(grad_2_part_1 * grad_2_part_2)
+        grad_2 = grad_2_part_3.T.dot(grad_2_part_1.dot(grad_2_part_2) )
 
-        grad_1_part_1 = (grad_2_part_1 * grad_2_part_2).dot(w2.T)
+        grad_1_part_1 = (grad_2_part_1.dot(grad_2_part_2)).dot(w2.T)
         grad_1_part_2 = d_tanh(layer_1)
-        grad_1_part_3 = X[i]
+        grad_1_part_3 = train_X[i]
 
         # print("1--",grad_1_part_1.shape, "  2--", grad_1_part_2.shape, "  3--", grad_1_part_3.shape)
 
-        grad_1_part_1_reshape = np.reshape(grad_1_part_1, (2, 2))
+        grad_1_part_1_reshape = np.reshape(grad_1_part_1, (14,14,3))
         grad_1_temp_1 = grad_1_part_1_reshape * grad_1_part_2
-        print(grad_1_temp_1.shape)
-        grad_1 = np.rot90(
-            signal.convolve2d(grad_1_part_3, np.rot90(grad_1_temp_1, 2), 'valid'),
-            2)
+        # print(grad_1_temp_1.shape)
+        grad_1 = np.zeros((3,3,3))
 
+        grad_1_part_3_t = np.zeros((30,30))
+        grad_1_part_3_t[1:29,1:29] = grad_1_part_3
+
+
+        for j in range(number_mask):
+            temp = np.zeros((28,28))
+            temp[1::2,1::2] = grad_1_temp_1[:,:,j]
+            grad_1[:, :, j] = np.rot90(
+                signal.convolve2d(grad_1_part_3_t, np.rot90(temp, 2), 'valid'),
+                2)
+        # if (i % 100 == 0)and(iter %10 ==0)and(iter>0):
+        #     print("grad1  norm:",np.sum(grad_1), "       grad2 norm", np.sum(grad_2))
         w2 = w2 - grad_2 * learning_rate
         w1 = w1 - grad_1 * learning_rate
 
 # ---- Cost after training ------
-for i in range(len(X)):
-    layer_1 = signal.convolve2d(X[i], w1, 'valid')
-    layer_1_act = tanh(layer_1)
-
+for i in range(len(train_X)):
+    for j in range(number_mask):
+        layer_1[:, :, j] = signal.convolve2d(train_X[i], w1[:, :, j], mode='same')[::2,::2]
+    layer_1_act = sigmoid(layer_1)
     layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+
     layer_2 = layer_1_act_vec.dot(w2)
-    layer_2_act = log(layer_2)
-    cost = np.square(layer_2_act - Y[i]).sum() * 0.5
+    layer_2_act = softmax(layer_2)
+    if i %100 ==0 :
+        print("different of the " ,i, "th training sample:" , np.round(layer_2_act-train_y[i], decimals=2), "   ")
+    cost = np.square(layer_2_act - train_y[i]).sum() * 0.5
     cost_after_train = cost_after_train + cost
-    final_out = np.append(final_out, layer_2_act)
+    # final_out = np.append(final_out, layer_2_act)
 
 # ----- Print Results ---
-print("\nW1 :", w1, "\n\nw2 :", w2)
+# print("\nW1 :", w1, "\n\nw2 :", w2)
 print("----------------")
 print("Cost before Training: ", cost_before_train)
 print("Cost after Training: ", cost_after_train)
 print("----------------")
-print("Start Out put : ", start_out)
-print("Final Out put : ", final_out)
-print("Ground Truth  : ", Y.T)
+# print("Start Out put : ", start_out)
+# print("Final Out put : ", final_out)
+# print("Ground Truth  : ", train_y.T)
 
 # -- end code --
--- last_year_oxford.py
+++ last_year_oxford.py
@@ -1,9 +1,8 @@
-import numpy as np, time
-from sklearn.datasets import make_classification
-import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
 
+np.random.seed(598765)
 
-# np.random.seed(3456789)
 
 def tanh(x):
     return np.tanh(x)
@@ -13,183 +12,132 @@ def d_tanh(x):
     return 1 - np.tanh(x) ** 2
 
 
-def sigmoid(x):
+def log(x):
     return 1 / (1 + np.exp(-1 * x))
 
 
-def d_sigmoid(x):
-    return sigmoid(x) * (1 - sigmoid(x))
+def d_log(x):
+    return log(x) * (1 - log(x))
+
+def loadMNIST( prefix, folder ):
+    intType = np.dtype( 'int32' ).newbyteorder( '>' )
+    nMetaDataBytes = 4 * intType.itemsize
+
+    data = np.fromfile( folder + "/" + prefix + '-images.idx3-ubyte', dtype = 'ubyte' )
+    magicBytes, nImages, width, height = np.frombuffer( data[:nMetaDataBytes].tobytes(), intType )
+    data = data[nMetaDataBytes:].astype( dtype = 'float32' ).reshape( [ nImages, width, height ] )
+
+    labels = np.fromfile( folder + "/" + prefix + '-labels.idx1-ubyte',
+                          dtype = 'ubyte' )[2 * intType.itemsize:]
+    return data, labels
+
+def mnist_data():
+    trainingImages, trainingLabels = loadMNIST( "train", "." )
+    testImages, testLabels = loadMNIST( "t10k", "." )
+    return trainingImages, trainingLabels
+
+nb_classes = 10
+def indices_to_one_hot(data, nb_classes):
+    """Convert an iterable of indices to one-hot encoded labels."""
+    targets = np.array(data).reshape(-1)
+    return np.eye(nb_classes)[targets]
+
+train_X, train_y = mnist_data()
+train_X = train_X /255
+
+x1 = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+x2 = np.array([[1, 1, 1], [0, 0, 0], [0, 0, 0]])
+x3 = np.array([[0, 0, 0], [1, 1, 1], [1, 1, 1]])
+x4 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+X = [x1, x2, x3, x4]
+Y = np.array([
+    [0.53],
+    [0.77],
+    [0.88],
+    [1.1]
+])
+
+# 0. Declare Weights
+w1 = np.random.randn(2,2)*4
+w2 = np.random.randn(4, 1) * 4
+number_mask = w1.shape[0]
 
 
-def ReLu(x):
-    return np.maximum(x, 0)
+# 1. Declare hyper Parameters
+num_epoch = 100
+learning_rate = 0.7
 
+cost_before_train = 0
+cost_after_train = 0
+final_out, start_out = np.array([[]]), np.array([[]])
+# ---- Cost before training ------
+for i in range(len(X)):
 
-def d_ReLu(x):
-    return (x > 0) * 1.0
+    layer_1 = signal.convolve2d(X[i], w1, 'valid')
+    layer_1_act = tanh(layer_1)
 
+    layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+    print("***************", layer_1_act_vec.shape   )
+    layer_2 = layer_1_act_vec.dot(w2)
+    layer_2_act = log(layer_2)
+    cost = np.square(layer_2_act - Y[i]).sum() * 0.5
+    cost_before_train = cost_before_train + cost
+    start_out = np.append(start_out, layer_2_act)
 
-num_epoch = 1000
-n_value_array = [0.01, 0.3, 1.0]
-sample_size = 500
-feature_size = 10
-useful_feature_size = 2
+# ----- TRAINING -------
+for iter in range(num_epoch):
 
-# making 2 class that relies on 2 imformative features
-# the explaination can be see :
-# https://scikit-learn.org/stable/auto_examples/datasets/plot_random_dataset.html
-X, Y = make_classification(n_samples=sample_size, n_features=feature_size,
-                           class_sep=0.04, n_redundant=0,
-                           n_informative=useful_feature_size, n_clusters_per_class=1)
-
-# Y = np.expand_dims(Y,axis=1)
-plt.scatter(X[:, 0], X[:, 1], c=Y)
-
-# adding an extra column so that the constant term could be trade equally
-X = np.c_[X, np.ones(sample_size)]
-Y = np.expand_dims(Y, axis=1)
-learing_rate = 0.05
-
-#  Declare Hyper parameter
-w1 = np.random.randn(feature_size + 1, 16)
-w2 = np.random.randn(16, 28)
-w3 = np.random.randn(28, 35)
-w4 = np.random.randn(35, 1)
-
-for n in n_value_array:
-    n_value = n
-
-    print("Current N Value : ", n_value)
-
-    # --------- NORMAL SGD Hyper Parameter Exact Same Weight-------------
-    normal_sgd_error = 100000
-
-    w1_normal = w1
-    history = w1
-    w2_normal = w2
-    w3_normal = w3
-    w4_normal = w4
-
-    # --------- ADDITIVE SGD Hyper Parameter Exact Same Weight-------------
-    ADDITIVE_sgd_error = 100000
-
-    w1_additive = w1
-    w2_additive = w2
-    w3_additive = w3
-    w4_additive = w4
-
-    # --------------- Training for normal---------------
-    for iter in range(num_epoch):
-        layer_1 = X.dot(w1_normal)
+    for i in range(len(X)):
+        layer_1 = signal.convolve2d(X[i], w1, 'valid')
         layer_1_act = tanh(layer_1)
 
-        layer_2 = layer_1_act.dot(w2_normal)
-        layer_2_act = sigmoid(layer_2)
+        layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+        layer_2 = layer_1_act_vec.dot(w2)
+        layer_2_act = log(layer_2)
 
-        layer_3 = layer_2_act.dot(w3_normal)
-        layer_3_act = tanh(layer_3)
+        cost = np.square(layer_2_act - Y[i]).sum() * 0.5
+        # print("Current iter : ",iter , " Current train: ",i, " Current cost: ",cost,end="\r")
 
-        layer_4 = layer_3_act.dot(w4_normal)
-        layer_4_act = sigmoid(layer_4)
+        grad_2_part_1 = layer_2_act - Y[i]
+        grad_2_part_2 = d_log(layer_2)
+        grad_2_part_3 = layer_1_act_vec
+        grad_2 = grad_2_part_3.T.dot(grad_2_part_1 * grad_2_part_2)
 
-        normal_sgd_error = np.square(layer_4_act - Y).sum() * 0.5
-        # print("Normal SGD Current iter : ",iter," Current Error: ", normal_sgd_error,end=' ')
-        # a_i = activation_i (z_i)
-        # z_i = W*a_(i-1)
+        grad_1_part_1 = (grad_2_part_1 * grad_2_part_2).dot(w2.T)
+        grad_1_part_2 = d_tanh(layer_1)
+        grad_1_part_3 = X[i]
 
-        dl_da4 = layer_4_act - Y
-        da4_dz4 = d_sigmoid(layer_4)
-        dl_dz4 = dl_da4 * da4_dz4
-        dz4_dW4 = layer_3_act
-        grad_4 = dz4_dW4.T.dot(dl_dz4)
+        # print("1--",grad_1_part_1.shape, "  2--", grad_1_part_2.shape, "  3--", grad_1_part_3.shape)
 
-        dl_da3 = (dl_dz4).dot(w4_normal.T)
-        da3_dz3 = d_tanh(layer_3)
-        dl_dz3 = dl_da3 * da3_dz3
-        dz3_dW3 = layer_2_act
-        grad_3 = dz3_dW3.T.dot(dl_dz3)
+        grad_1_part_1_reshape = np.reshape(grad_1_part_1, (2, 2))
+        grad_1_temp_1 = grad_1_part_1_reshape * grad_1_part_2
+        grad_1 = np.rot90(
+            signal.convolve2d(grad_1_part_3, np.rot90(grad_1_temp_1, 2), 'valid'),
+            2)
 
-        dl_da2 = (dl_dz3).dot(w3_normal.T)
-        da2_dz2 = d_sigmoid(layer_2)
-        dl_dz2 = dl_da2 * da2_dz2
-        dz2_dW2 = layer_1_act
-        grad_2 = dz2_dW2.T.dot(dl_dz2)
+        w2 = w2 - grad_2 * learning_rate
+        w1 = w1 - grad_1 * learning_rate
 
-        dl_da1 = (dl_dz2).dot(w2_normal.T)
-        da1_dz1 = d_tanh(layer_1)
-        dl_dz1 = dl_da1 * da1_dz1
-        dz1_dW1 = X
-        grad_1 = dz1_dW1.T.dot(dl_dz1)
+# ---- Cost after training ------
+for i in range(len(X)):
+    layer_1 = signal.convolve2d(X[i], w1, 'valid')
+    layer_1_act = tanh(layer_1)
 
-        w1_normal = w1_normal - learing_rate * grad_1
-        w2_normal = w2_normal - learing_rate * grad_2
-        w3_normal = w3_normal - learing_rate * grad_3
-        w4_normal = w4_normal - learing_rate * grad_4
+    layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+    layer_2 = layer_1_act_vec.dot(w2)
+    layer_2_act = log(layer_2)
+    cost = np.square(layer_2_act - Y[i]).sum() * 0.5
+    cost_after_train = cost_after_train + cost
+    final_out = np.append(final_out, layer_2_act)
 
-        if (iter % 100 == 0) and (iter > 0):
-            print("Error for Normal SGD Current iter: ", iter, "  Error: ", normal_sgd_error)
-            print(np.square(history - w1_normal).sum())
-            history = w1_normal
-
-    print("Final Error for Normal SGD Current iter: ", iter, "  Error: ", np.round(normal_sgd_error, 4))
-    print("\n---------------------------------")
-
-    # --------------- Training for Additive---------------
-    for iter in range(num_epoch):
-        layer_1 = X.dot(w1_additive)
-        layer_1_act = tanh(layer_1)
-
-        layer_2 = layer_1_act.dot(w2_additive)
-        layer_2_act = sigmoid(layer_2)
-
-        layer_3 = layer_2_act.dot(w3_additive)
-        layer_3_act = tanh(layer_3)
-
-        layer_4 = layer_3_act.dot(w4_additive)
-        layer_4_act = sigmoid(layer_4)
-
-        ADDITIVE_sgd_error = np.square(layer_4_act - Y).sum() * 0.5
-        # print("Additive Current iter : ",iter," Current Error: ", ADDITIVE_sgd_error,end='\r')
-
-        dl_da4 = layer_4_act - Y
-        da4_dz4 = d_sigmoid(layer_4)
-        dl_dz4 = dl_da4 * da4_dz4
-        dz4_dW4 = layer_3_act
-        grad_4 = dz4_dW4.T.dot(dl_dz4)
-
-        dl_da3 = (dl_dz4).dot(w4_additive.T)
-        da3_dz3 = d_tanh(layer_3)
-        dl_dz3 = dl_da3 * da3_dz3
-        dz3_dW3 = layer_2_act
-        grad_3 = dz3_dW3.T.dot(dl_dz3)
-
-        dl_da2 = (dl_dz3).dot(w3_additive.T)
-        da2_dz2 = d_sigmoid(layer_2)
-        dl_dz2 = dl_da2 * da2_dz2
-        dz2_dW2 = layer_1_act
-        grad_2 = dz2_dW2.T.dot(dl_dz2)
-
-        dl_da1 = (dl_dz2).dot(w2_additive.T)
-        da1_dz1 = d_tanh(layer_1)
-        dl_dz1 = dl_da1 * da1_dz1
-        dz1_dW1 = X
-        grad_1 = dz1_dW1.T.dot(dl_dz1)
-
-        # ------ Calculate The Additive weight -------
-        ADDITIVE_NOISE_STD = n_value / (np.power((1 + iter), 0.55))
-        ADDITIVE_GAUSSIAN_NOISE = np.random.normal(loc=0, scale=ADDITIVE_NOISE_STD)
-        # ------ Calculate The Additive weight -------
-
-        w1_additive = w1_additive - learing_rate * (grad_1 + ADDITIVE_GAUSSIAN_NOISE)
-        w2_additive = w2_additive - learing_rate * (grad_2 + ADDITIVE_GAUSSIAN_NOISE)
-        w3_additive = w3_additive - learing_rate * (grad_3 + ADDITIVE_GAUSSIAN_NOISE)
-        w4_additive = w4_additive - learing_rate * (grad_4 + ADDITIVE_GAUSSIAN_NOISE)
-
-        if (iter % 100 == 0) and (iter > 0):
-            print("Error for Noisy SGD Current iter: ", iter, "  Error: ", np.round(ADDITIVE_sgd_error, 4))
-
-    print("Final Error for Noisy SGD Current iter: ", iter, "  Error: ", np.round(ADDITIVE_sgd_error, 4))
-    print("\n---------------------------------")
-plt.show()
+# ----- Print Results ---
+print("\nW1 :", w1, "\n\nw2 :", w2)
+print("----------------")
+print("Cost before Training: ", cost_before_train)
+print("Cost after Training: ", cost_after_train)
+print("----------------")
+print("Start Out put : ", start_out)
+print("Final Out put : ", final_out)
+print("Ground Truth  : ", Y.T)
 
 # -- end code --
