8af9303570da6db335e191a30eb5c34849b2c3dd
Ruofeng <geo0105824@gmail.com>
2019-02-14 13:28:46 +0000

    averaging error over 20 runs.

--- convolutional_from_stratch.py
+++ convolutional_from_stratch.py
@@ -87,84 +87,12 @@ number_mask = w1.shape[0]
 layer_1 = np.zeros((14, 14, 3))
 
 # 1. Declare hyper Parameters
-num_epoch = 200
+num_epoch = 21
 learning_rate = 1
 c_value = [0.2 * i for i in range(16)]
 d_value = list(range(1, 6))
-cost_before_train = 0
-cost_after_train = 0
-
-
-def train_convolutional_network(train_X=train_X, train_y=train_y.copy(), noise="NEM noise", learning_rate_=0.1,
-                                num_epoch_=num_epoch, w1_=w1, w2_=w2, layer_1=layer_1,
-                                c=0.5, d=5):
-    # ----- TRAINING -------
-    print("start training--------------")
-    error_list = []
-    train_size_ = len(train_X)
-    for r in range(num_epoch_):
-        if r % 700 == 0:
-            learning_rate_ = learning_rate_ * 0.5
-        # print("------------iteration:", iter, "-----------")
-        for i in range(train_size_):
-            for j in range(number_mask):
-                layer_1[:, :, j] = signal.convolve2d(train_X[i], w1_[:, :, j], mode='same')[::2, ::2]
-            layer_1_act = sigmoid(layer_1)
-            layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
-
-            layer_2 = layer_1_act_vec.dot(w2_)
-            layer_2_act = softmax(layer_2)
-            
-            
-            
-            # adding noise
-            # new_y = train_y[i]
-            if noise == "NEM noise":
-                c_t_d = 0.5 * math.sqrt(c / ((i + 1 + train_size_ * r) ** d))
-                uniform_noise = np.random.uniform(-c_t_d, c_t_d, (1, 10))
-                if uniform_noise.dot(np.log(layer_2_act).T) > 0:
-                    # new_y = train_y[i] + uniform_noise
-                    train_y[i] = train_y[i] + uniform_noise
-            #
-            # grad_2_part_1 = layer_2_act - new_y
-            grad_2_part_1 = layer_2_act - train_y[i]
-            grad_2_part_2 = softmax_grad(layer_2_act)
-            grad_2_part_3 = layer_1_act_vec
-            grad_2 = grad_2_part_3.T.dot(grad_2_part_1.dot(grad_2_part_2))
-
-            grad_1_part_1 = (grad_2_part_1.dot(grad_2_part_2)).dot(w2.T)
-            grad_1_part_2 = d_sigmoid(layer_1)
-            grad_1_part_3 = train_X[i]
-            
-            # print("1--",grad_1_part_1.shape, "  2--", grad_1_part_2.shape, "  3--", grad_1_part_3.shape)
-
-            grad_1_part_1_reshape = np.reshape(grad_1_part_1, (14, 14, 3))
-            grad_1_temp_1 = grad_1_part_1_reshape * grad_1_part_2
-            # print(grad_1_temp_1.shape)
-            grad_1 = np.zeros((3, 3, 3))
-
-            grad_1_part_3_t = np.zeros((30, 30))
-            grad_1_part_3_t[1:29, 1:29] = grad_1_part_3
-
-            for j in range(number_mask):
-                temp = np.zeros((28, 28))
-                temp[1::2, 1::2] = grad_1_temp_1[:, :, j]
-                grad_1[:, :, j] = np.rot90(
-                    signal.convolve2d(grad_1_part_3_t, np.rot90(temp, 2), 'valid'),
-                    2)
-            # if (i % 100 == 0)and(iter %10 ==0)and(iter>0):
-            #     print("grad1  norm:",np.sum(grad_1), "       grad2 norm", np.sum(grad_2))
-            w2_ = w2_ - grad_2 * learning_rate_
-            w1_ = w1_ - grad_1 * learning_rate_
-        error_list.append(train_set_error_rate(w1_=w1_, w2_ = w2_))
-        
-    return w1_, w2_,error_list
-
-
-start_time = time.time()
-w1_without_noise, w2_without_noise, error_no_noise = train_convolutional_network(noise="None")
-w1, w2, error_noise = train_convolutional_network()
-print("running time:", round(time.time() - start_time, 2))
+# cost_before_train = 0
+# cost_after_train = 0
 
 
 def train_set_error_rate(w1_=w1, w2_=w2, train_X_=train_X, train_y_=train_y, layer_1=layer_1):
@@ -185,8 +113,88 @@ def train_set_error_rate(w1_=w1, w2_=w2, train_X_=train_X, train_y_=train_y, lay
     return error_rate
 
 
-plt.plot(range(num_epoch), error_no_noise, marker = 'o', label = "No Noise")
-plt.plot(range(num_epoch), error_noise, marker = '', label = "Noisy")
+print("start training--------------")
+
+
+def train_convolutional_network(train_X=train_X, train_y=train_y.copy(), noise="NEM noise", learning_rate_=0.1,
+                                num_epoch_=num_epoch, w1_=w1, w2_=w2, layer_1=layer_1,
+                                c=0.5, d=5):
+    # ----- TRAINING -------
+    error_list = []
+    train_size_ = len(train_X)
+    for r in range(num_epoch_):
+        if r % 700 == 0:
+            learning_rate_ = learning_rate_ * 0.5
+        # print("------------iteration:", iter, "-----------")
+        for i in range(train_size_):
+            for j in range(number_mask):
+                layer_1[:, :, j] = signal.convolve2d(train_X[i], w1_[:, :, j], mode='same')[::2, ::2]
+            layer_1_act = sigmoid(layer_1)
+            layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
+
+            layer_2 = layer_1_act_vec.dot(w2_)
+            layer_2_act = softmax(layer_2)
+
+            # adding noise
+            # new_y = train_y[i]
+            if noise == "NEM noise":
+                c_t_d = 0.5 * math.sqrt(c / ((i + 1 + train_size_ * r) ** d))
+                uniform_noise = np.random.uniform(-c_t_d, c_t_d, (1, 10))
+                if uniform_noise.dot(np.log(layer_2_act).T) > 0:
+                    # new_y = train_y[i] + uniform_noise
+                    train_y[i] = train_y[i] + uniform_noise
+            #
+            # grad_2_part_1 = layer_2_act - new_y
+            grad_2_part_1 = layer_2_act - train_y[i]
+            grad_2_part_2 = softmax_grad(layer_2_act)
+            grad_2_part_3 = layer_1_act_vec
+            grad_2 = grad_2_part_3.T.dot(grad_2_part_1.dot(grad_2_part_2))
+
+            grad_1_part_1 = (grad_2_part_1.dot(grad_2_part_2)).dot(w2.T)
+            grad_1_part_2 = d_sigmoid(layer_1)
+            grad_1_part_3 = train_X[i]
+
+            # print("1--",grad_1_part_1.shape, "  2--", grad_1_part_2.shape, "  3--", grad_1_part_3.shape)
+
+            grad_1_part_1_reshape = np.reshape(grad_1_part_1, (14, 14, 3))
+            grad_1_temp_1 = grad_1_part_1_reshape * grad_1_part_2
+            # print(grad_1_temp_1.shape)
+            grad_1 = np.zeros((3, 3, 3))
+
+            grad_1_part_3_t = np.zeros((30, 30))
+            grad_1_part_3_t[1:29, 1:29] = grad_1_part_3
+
+            for j in range(number_mask):
+                temp = np.zeros((28, 28))
+                temp[1::2, 1::2] = grad_1_temp_1[:, :, j]
+                grad_1[:, :, j] = np.rot90(
+                    signal.convolve2d(grad_1_part_3_t, np.rot90(temp, 2), 'valid'),
+                    2)
+            # if (i % 100 == 0)and(iter %10 ==0)and(iter>0):
+            #     print("grad1  norm:",np.sum(grad_1), "       grad2 norm", np.sum(grad_2))
+            w2_ = w2_ - grad_2 * learning_rate_
+            w1_ = w1_ - grad_1 * learning_rate_
+        error_list.append(train_set_error_rate(w1_=w1_, w2_=w2_))
+
+    return w1_, w2_, error_list
+
+
+start_time = time.time()
+noisy_error_history = [0] * num_epoch
+noiseless_error_history = [0] * num_epoch
+
+for i in range(20):
+    w1_without_noise, w2_without_noise, error_noiseless = train_convolutional_network(noise="None")
+    w1, w2, error_noisy = train_convolutional_network()
+    noisy_error_history = [a + b for a, b in zip(noisy_error_history, error_noisy)]
+    noiseless_error_history = [a + b for a, b in zip(noiseless_error_history, error_noiseless)]
+noisy_error_history = [a/20 for a in noisy_error_history]
+noiseless_error_history = [a/20 for a in noiseless_error_history]
+
+print("running time:", round(time.time() - start_time, 2))
+
+plt.plot(range(1,num_epoch+1), noisy_error_history, marker='', color='blue', label="Noisy")
+plt.plot(range(1,num_epoch+1), noiseless_error_history, marker='', color='red', label="No Noise")
 plt.legend()
 plt.show()
 # ----- Print Results ---
