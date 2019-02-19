re-evaluate the paper "Noise-enhanced neural network", with correct understanding on "down-sampling with factor 2". corrected mistake in convoluutional_from_stratch

convolution_with_pooling.py
@@ -3,7 +3,7 @@ import time
 import math
 from scipy import signal
 import matplotlib.pyplot as plt
-
+import skimage.measure
 
 # np.random.seed(13579)
 
@@ -84,10 +84,11 @@ train_y = train_y[:train_size]
 w1 = np.random.randn(3, 3, 3) * 3
 w2 = np.random.randn(14 * 14 * 3, 10)
 number_mask = w1.shape[0]
-layer_1 = np.zeros((14, 14, 3))
+layer_1 = np.zeros((28, 28, 3))
+
 
 # 1. Declare hyper Parameters
-num_epoch = 50
+num_epoch = 20
 learning_rate = 1
 c_value = [0.2 * i for i in range(16)]
 d_value = list(range(1, 6))
@@ -95,16 +96,18 @@ d_value = list(range(1, 6))
 # cost_after_train = 0
 
 
-def train_set_error_rate(w1_=w1, w2_=w2, train_X_=train_X, train_y_=train_y, layer_1=layer_1):
+def train_set_error_rate(w1_=w1, w2_=w2,train_X_=train_X, train_y_=train_y, layer_1=layer_1 ):
     count_error = 0
     # ---- Cost after training ------
     for i in range(len(train_X_)):
         for j in range(number_mask):
-            layer_1[:, :, j] = signal.convolve2d(train_X[i], w1_[:, :, j], mode='same')[::2, ::2]
+            layer_1[:, :, j] = signal.convolve2d(train_X[i], w1_[:, :, j], mode='same')
         layer_1_act = sigmoid(layer_1)
-        layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
 
-        layer_2 = layer_1_act_vec.dot(w2_)
+        layer_1_pooled = skimage.measure.block_reduce(layer_1_act, (2, 2,1), func=np.mean)
+        layer_1_pooled_vec = np.expand_dims(np.reshape(layer_1_pooled, -1), axis=0)
+
+        layer_2 = layer_1_pooled_vec.dot(w2_)
         layer_2_act = softmax(layer_2)
         # if i % 100 == 0:
         #     print("different of the ", i, "th training sample:", np.round(layer_2_act - train_y[i], decimals=2), "   ")
@@ -128,47 +131,51 @@ def train_convolutional_network(train_X=train_X, train_y=train_y.copy(), noise="
         # print("------------iteration:", iter, "-----------")
         for i in range(train_size_):
             for j in range(number_mask):
-                layer_1[:, :, j] = signal.convolve2d(train_X[i], w1_[:, :, j], mode='same')[::2, ::2]
+                layer_1[:, :, j] = signal.convolve2d(train_X[i], w1_[:, :, j], mode='same')
             layer_1_act = sigmoid(layer_1)
-            layer_1_act_vec = np.expand_dims(np.reshape(layer_1_act, -1), axis=0)
 
-            layer_2 = layer_1_act_vec.dot(w2_)
+            layer_1_pooled = skimage.measure.block_reduce(layer_1_act, (2, 2, 1), func=np.mean)
+            layer_1_pooled_vec = np.expand_dims(np.reshape(layer_1_pooled, -1), axis=0)
+
+            layer_2 = layer_1_pooled_vec.dot(w2_)
             layer_2_act = softmax(layer_2)
 
             # adding noise
-            new_y = train_y[i]
+            # new_y = train_y[i]
             if noise == "NEM noise":
                 c_t_d = 0.5 * math.sqrt(c / ((i + 1 + train_size_ * r) ** d))
                 uniform_noise = np.random.uniform(-c_t_d, c_t_d, (1, 10))
                 if uniform_noise.dot(np.log(layer_2_act).T) > 0:
-                    new_y = train_y[i] + uniform_noise
-                    # train_y[i] = train_y[i] + uniform_noise
+                    # new_y = train_y[i] + uniform_noise
+                    train_y[i] = train_y[i] + uniform_noise
             #
-            grad_2_part_1 = layer_2_act - new_y
-            # grad_2_part_1 = layer_2_act - train_y[i]
+            # grad_2_part_1 = layer_2_act - new_y
+            grad_2_part_1 = layer_2_act - train_y[i]
             grad_2_part_2 = softmax_grad(layer_2_act)
-            grad_2_part_3 = layer_1_act_vec
+            grad_2_part_3 = layer_1_pooled_vec
             grad_2 = grad_2_part_3.T.dot(grad_2_part_1.dot(grad_2_part_2))
 
-            grad_1_part_1 = (grad_2_part_1.dot(grad_2_part_2)).dot(w2_.T)
+            # differentiation from before
+            grad_1_part_1_temp = np.reshape((grad_2_part_1.dot(grad_2_part_2)).dot(w2_.T), (14,14,3))
+            grad_1_window = grad_1_part_1_temp.repeat(2, axis=0).repeat(2,axis=1)       #expand dimension for pool
+            grad_1_mask = np.full((28,28,3), 0.25, dtype=float)
+
+            grad_1_part_1 = grad_1_window * grad_1_mask                                 # shape = (28,28,3)
             grad_1_part_2 = d_sigmoid(layer_1)
-            grad_1_part_3 = train_X[i]
+
+            grad_1_part_3 = np.zeros((30, 30))
+            grad_1_part_3[1:29, 1:29] = train_X[i]
+
 
             # print("1--",grad_1_part_1.shape, "  2--", grad_1_part_2.shape, "  3--", grad_1_part_3.shape)
 
-            grad_1_part_1_reshape = np.reshape(grad_1_part_1, (14, 14, 3))
-            grad_1_temp_1 = grad_1_part_1_reshape * grad_1_part_2
-            # print(grad_1_temp_1.shape)
+            temp = grad_1_part_1 * grad_1_part_2
+            # print("shape_temp-------", temp.shape, "grad_1_part_3----------", grad_1_part_3.shape )
             grad_1 = np.zeros((3, 3, 3))
 
-            grad_1_part_3_t = np.zeros((30, 30))
-            grad_1_part_3_t[1:29, 1:29] = grad_1_part_3
-
             for j in range(number_mask):
-                temp = np.zeros((28, 28))
-                temp[1::2, 1::2] = grad_1_temp_1[:, :, j]
                 grad_1[:, :, j] = np.rot90(
-                    signal.convolve2d(grad_1_part_3_t, np.rot90(temp, 2), 'valid'),
+                    signal.convolve2d(grad_1_part_3, np.rot90(temp[:,:,j], 2), mode='valid'),
                     2)
             # if (i % 100 == 0)and(iter %10 ==0)and(iter>0):
             #     print("grad1  norm:",np.sum(grad_1), "       grad2 norm", np.sum(grad_2))
@@ -183,7 +190,7 @@ start_time = time.time()
 noisy_error_history = [0] * num_epoch
 noiseless_error_history = [0] * num_epoch
 
-for i in range(5):
+for i in range(1):
     w1_without_noise, w2_without_noise, error_noiseless = train_convolutional_network(noise="None")
     w1, w2, error_noisy = train_convolutional_network()
     noisy_error_history = [a + b for a, b in zip(noisy_error_history, error_noisy)]